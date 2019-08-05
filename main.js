8533dbe53473db870bd156a988f7cd2cf603586a
muki12321 <muhammed10200802@outlook.com>
2019-08-06 00:54:33 +0200

    Nisam uspio ali nema predaje, novi dan nova nafaka

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var email1 = document.getElementById('email');
 var pass = document.getElementById('password'); 
 var send = document.getElementById('submit');
+
 var uspjesnaPrijava;
 
 send.addEventListener('click', validacija);
@@ -11,29 +12,34 @@ pass.addEventListener('focus',clear);
 function validacija () {
 	uspjesnaPrijava = 1;
 	//email 
-	//FIXME: it have to contains @ but not gmail... its optional 
-	//FIXME: use === instead of == everywhere, explain me why
+	//FIXME: it have to contains @ but not gmail... its optional --> DONE
+
+	//FIXME: use === instead of == everywhere, explain me why 
+		// == is equal to
+		// === is equal to and equal to same type 
+		// example : 0 == false --> true
+		//			 0 === false --> false because 0 is a number, an integer and false is a boolean
 
 
-
-	if (email1.value.indexOf('gmail') == -1) {
+	if (email1.value.indexOf('@gmail') == -1) { 
 		email1.value = 'Email mora biti GMAIL'
 		uspjesnaPrijava = 0;
 	};
 	if (email1.value == '') {
 		email1.value = 'Ovo polje je obavezno';
-		//FIXME: it is not your job... to alert user in ant way
-		email1.style.color = 'red';
+		//FIXME: it is not your job... to alert user in ant way --> DONE
+		//email1.style.color = 'red'; DELETE
 		uspjesnaPrijava = 0;
 	};
 
 	//password
-	//FIXME: don't check if field is empty... just use html attribute 'required'
+	//FIXME: don't check if field is empty... just use html attribute 'required' 
 	if (pass.value == '') {
-		//FIXME: ??? wrong way .... it must not use value for warnings
-		pass.value = 'Ovo polje je prazno';
+		//FIXME: ??? wrong way .... it must not use value for warnings --> DONE
+		//pass.value = 'Ovo polje je prazno'; DELETE
+
 		//FIXME: ?????????
-		pass.style.color = 'red';
+		//pass.style.color = 'red'; DELETE
 		uspjesnaPrijava = 0;	
 	};
 
---
+++ main2.js
@@ -0,0 +1,7 @@
+var email1 = document.getElementById('email');
+var pass = document.getElementById('password'); 
+
+console.log(email1);
+
+
+
